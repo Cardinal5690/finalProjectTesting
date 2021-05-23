ed2d6140d0476e2bc5753d7ccaaf89033c04d922
Ihor Karpenko <igor5690@ukr.net>
2021-05-23 21:21:41 +0300

    Delete answer class.

--- src/main/java/com/testing/dao/AnswerDao.java
+++
@@ -1,9 +0,0 @@
-package com.testing.dao;
-
-import com.testing.model.entity.Answer;
-
-import java.util.List;
-
-public interface AnswerDao extends GenericDao<Answer> {
-    List<Answer> findAllByQuestionId(int questionId);
-}
